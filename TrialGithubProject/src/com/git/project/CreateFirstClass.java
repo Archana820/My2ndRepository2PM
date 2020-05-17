dac35353a6be6975d294c9c9e5f6422c5287f679
Archana820 <d.archemail25@gmail.com>
2020-05-18 00:51:28 +0100

    Added a,b,c in First class

--- TrialGithubProject/src/com/git/project/CreateFirstClass.java
+++ TrialGithubProject/src/com/git/project/CreateFirstClass.java
@@ -1,7 +1,7 @@
 package com.git.project;
 
 public class CreateFirstClass {
-
+	int a=10,b=20,c;
 	public static void main(String[] args) {
 		System.out.println("My 1st project in git repo");
         System.out.println("Added new text");
