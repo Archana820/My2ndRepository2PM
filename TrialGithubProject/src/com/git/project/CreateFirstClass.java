8a3f5717636ed7576abe6b5117484a4e58fcc20e
Archana820 <d.archemail25@gmail.com>
2020-05-18 01:19:40 +0100

    addition in first class

--- TrialGithubProject/src/com/git/project/CreateFirstClass.java
+++ TrialGithubProject/src/com/git/project/CreateFirstClass.java
@@ -1,13 +1,16 @@
 package com.git.project;
 
 public class CreateFirstClass {
-	int a=10,b=20,sum;
+	
 	
 	public static void main(String[] args) {
+		int a=10,b=20,sum=0;
+		
 		System.out.println("My 1st project in git repo");
         System.out.println("Added new text");
         System.out.println("try it!!");
         System.out.println("Added 4th time");
+        sum=a+b;
 	}
 
 }
